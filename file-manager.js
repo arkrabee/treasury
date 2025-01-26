1b1c9ca2546e39f8618644ce3e896573ad27de98
Abdullah Rabee <arkrabee@gmail.com>
2025-01-26 16:59:06 +0530

    Update file-manager.js

--- file-manager.js
+++ file-manager.js
@@ -2,36 +2,69 @@ const folderStructure = {
     name: "Home",
     type: "folder",
     children: [
-        {
-            name: "Links",
-            type: "folder",
-            children: [
-                { name: "Resume.docx", type: "file" },
-                { name: "CoverLetter.docx", type: "file", link: "googe.com"},
-            ],
-        },
+
 
         {
-            name: "Sahal and Shammas",
-            type: "folder",
-            children: [
-                { name: "sahal.docx", type: "file" },
-                { name: "shammas.docx", type: "file" },
-                { name: "super.jpg", type: "file" },
-            ],
-        },
-        
-        {
-            name: "Documents",
+            name: "Hero",
             type: "folder",
             children: [
                 {
-                    name: "blogs",
-                    type: "file",
-                    link: "[main]",
-                }
-            ]
+                    name: "Tool - 1", 
+                    type: "folder",
+                    children: [
+                        {
+                            name: "Tool - 1", 
+                            type: "folder",
+                            children: [
+                                
+                            ]
+                        },
+                        {
+                            name: "Tool - 10", 
+                            type: "folder",
+                            children: [
+                                
+                            ]
+                        },
+                        {
+                            name: "Tool - 7024", 
+                            type: "folder",
+                            children: [
+                                
+                            ]
+                        },
+                        {
+                            name: "Tool - 7025", 
+                            type: "folder",
+                            children: [
+                                {name: "drive.links", type: "file", link: `[<a href="https://drive.google.com/drive/folders/1L1ZJ5c3kuKmJLQKavDLg3_BGjan_XCul">https://drive.google.com/drive/folders/1L1ZJ5c3kuKmJLQKavDLg3_BGjan_XCul</a>]` }
+                            ],
+                        },
+                    ]
+                },
+                {
+                    name: "Tool - 4545", 
+                    type: "folder",
+                    children: [
 
+                    ]
+                },
+                {
+                    name: "Portal - code: 23445", 
+                    type: "folder",
+                    children: [
+
+                    ]
+                },
+                {
+                    name: "Super Logger", 
+                    type: "folder",
+                    children: [
+
+                    ]
+                },
+                
+            ]
         },
         
     ]
